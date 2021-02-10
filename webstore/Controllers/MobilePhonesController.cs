32926743b50e3e4cacd223c403bf6b24b7ccae20
redeveight <redeveight@gmail.com>
2021-02-10 12:55:26 +0300

    feat: add MobilePhones Controller

---
+++ webstore/Controllers/MobilePhonesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using webstore.Data.Interfaces;
+
+namespace webstore.Controllers
+{
+    public class MobilePhonesController : Controller
+    {
+        private readonly IAllMobilePhones _allMobilePhones;
+        private readonly IMobilePhonesCategory _allCategories;
+
+        public MobilePhonesController(IAllMobilePhones iallMobilePhones, IMobilePhonesCategory iMobilePhonesCategory)
+        {
+            _allMobilePhones = iallMobilePhones;
+            _allCategories = iMobilePhonesCategory;
+        }
+
+        public ViewResult List()
+        {
+            var mobilePhones = _allMobilePhones.MobilePhones;
+            return View(mobilePhones);
+        }
+    }
+}
--- webstore/Startup.cs
+++ webstore/Startup.cs
@@ -7,6 +7,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using webstore.Data.Interfaces;
+using webstore.Data.mocks;
 
 namespace webstore
 {
@@ -16,6 +18,8 @@ namespace webstore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IAllMobilePhones, MockMobilePhones>();
+            services.AddTransient<IMobilePhonesCategory, MockCategory>();
             services.AddMvc();
         }
 
