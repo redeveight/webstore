9dcd098dc1ec17e385720cac9a5d2b292d353905
redeveight <redeveight@gmail.com>
2021-02-10 03:32:28 +0300

    feat: add basic models

---
+++ webstore/Data/Models/Smartphone.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webstore.Data.Models
+{
+    public class Smartphone
+    {
+        public int id { set; get; }
+
+        public string modelName { set; get; }
+
+        public string description { set; get; }
+
+        public string shortDescription { set; get; }
+
+        public string image { set; get; }
+
+        public ushort price { set; get; }
+
+        public bool isFavourite { set; get; }
+
+        public int available { set; get; }
+
+        public int categoryID { set; get; }
+
+        public virtual Category Category { set; get; }
+    }
+}
